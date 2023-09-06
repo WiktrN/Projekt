8c45a51d470132a7fda630a2e58e6604441445ad
Wiktor Nath <wiktornath@MacBook-Air-Wiktor.local>
2023-09-06 12:58:03 +0200

    Kalkulator

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
-  public static void main(String[] args){
-    System.out.print("Hello World");
-  }
+    public static void main(String[] args){
+    Kalkulator dupa = new Kalkulator();
+    
+    
+    dupa.addiction(2, 3);    
+
+    dupa.subtraction(10, 3);
+
+    dupa.multiplication(4, 4);
+    
+    dupa.division(0, 0);
+    }
 }
