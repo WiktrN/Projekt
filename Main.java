9f2b34f1ab93359b86e1dca66bccfc19bdf9d7ea
Wiktor Nath <wiktornath@MacBook-Air-Wiktor.local>
2023-09-08 11:03:57 +0200

    kalkulator

--- Main.java
+++ Main.java
@@ -4,23 +4,29 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int a;
-        int b;
-        String c;
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
+        String x;
+        do {
+            String c;
+            int b;
+            int a;
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
 
-        System.out.print("Enter calculation type: ");
-        c = reader.readLine();
-        System.out.print("Enter first number: ");
-        a = Integer.parseInt(reader.readLine());
-        System.out.print("Enter second number: ");
-        b = Integer.parseInt(reader.readLine());
+            System.out.print("Enter calculation type: ");
+            c = reader.readLine();
+            System.out.print("Enter first number: ");
+            a = Integer.parseInt(reader.readLine());
+            System.out.print("Enter second number: ");
+            b = Integer.parseInt(reader.readLine());
 
-        Kalkulator wynik;
-        wynik = new Kalkulator();
-        int suma = wynik.calculate(a, b, c);
+            Kalkulator wynik = new Kalkulator();
+            int suma = wynik.calculate(a, b, c);
 
-        System.out.println("Your result is: " + suma);
+            System.out.println("Your result is: " + suma + '\n');
+
+            System.out.print("Do you want to continue Y/n ");
+            x = reader.readLine();
+
+        } while (x.equals("y"));
     }
 }
